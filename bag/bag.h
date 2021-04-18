e778ee10c506eaad1830c2beab4f4c4419d59a52
David Kotz <David.F.Kotz@dartmouth.edu>
2021-04-18 14:29:02 -0400

    replace non-ascii characters

--- bag/bag.h
+++ bag/bag.h
@@ -1,7 +1,7 @@
 /* 
  * bag.h - header file for CS50 'bag' module
  * 
- * A ‘bag’ is a collection of indistinguishable items; it starts empty, 
+ * A 'bag' is a collection of indistinguishable items; it starts empty, 
  * grows as the caller adds one item at a time, and shrinks as the caller 
  * extracts one item at a time. It could be empty, or could contain hundreds
  * of items. Since items are indistinguishable, the module is free to return
